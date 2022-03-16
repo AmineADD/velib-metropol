14eda5315cb55ef68195c8f1ba199ad677987735
Your-name <amine.addajou@gmail.com>
2022-03-16 13:30:52 +0100

    use MakeStyles mui

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Styles from './NavBar.module.css'
+import useStyles from './styles';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import Status from './Status/Status';
@@ -13,15 +13,15 @@ import { Typography } from '@mui/material';
 
 const NavBar: FC = () => {
     const { isConnected, isDarkMode } = useAppCreateContext();
-
+    const classes = useStyles();
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" className={`${Styles.AppBar} ${isDarkMode ? Styles.dark : Styles.transparent}`}>
+            <AppBar position="static" className={`${classes.AppBar} ${isDarkMode ? classes.dark : classes.transparent}`}>
                 <Mode />
-                <Typography variant='h5' className={`${Styles.title} ${isDarkMode ? Styles.dark : Styles.transparent}`}>Vélib&apos; API</Typography>
-                <Toolbar className={Styles.toolTip}>
+                <Typography variant='h5' className={`${classes.title} ${isDarkMode ? classes.dark : classes.transparent}`}>Vélib&apos; API</Typography>
+                <Toolbar className={classes.toolTip}>
                     <Stack direction="row">
-                        <Chip className={`${isDarkMode ? Styles.Chipdark : Styles.Chiptransparent}`} avatar={<Status status={isConnected} />} label={isConnected ? 'connected' : 'not connected'} />
+                        <Chip className={`${isDarkMode ? classes.Chipdark : classes.Chiptransparent}`} avatar={<Status status={isConnected} />} label={isConnected ? 'connected' : 'not connected'} />
                     </Stack>
                 </Toolbar>
             </AppBar>
---
+++ src/components/NavBar/styles.ts
@@ -0,0 +1,34 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles({
+    AppBar: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+
+    toolTip: {
+        display: "flex",
+        flexDirection: "row-reverse"
+    },
+    title: {
+        color: "black",
+    },
+    transparent: {
+        backgroundColor: "transparent",
+        color: "black"
+    },
+    dark: {
+        backgroundColor: "#121212",
+        color: "white",
+    },
+    Chiptransparent: {
+        backgroundColor: "rgba(0, 0, 0, 0.08)",
+        color: "black"
+    },
+    Chipdark: {
+        backgroundColor: "rgba(163, 144, 144, 0.08)",
+        color: "white"
+    }
+});
